3a7e30b7e2bce6957f354a834d6a30854ef4eb1c
martinmramos <xitej091@gmail.com>
2022-06-28 17:41:34 +0200

    Constructores clases Coche, Exposicion y Reparacion

--- src/com/company/Coche.java
+++ src/com/company/Coche.java
@@ -1,4 +1,34 @@
 package com.company;
 
 public class Coche {
+    private String marca;
+    private String modelo;
+    private String matricula;
+    private double precioCompra;
+    private double precioVenta;
+    private String tipo;
+
+    private Exposicion exposicion;
+
+    private boolean reservado;
+    private boolean comprado;
+    private boolean vendido;
+
+    public Coche(String marca, String modelo, String matricula, double precioCompra, double precioVenta, String tipo, Exposicion exposicion) throws ExceptionParametrosInvalidos {
+        if (marca == null) throw new ExceptionParametrosInvalidos("La marca no puede ser null.");
+        this.marca = marca;
+        if (modelo == null) throw new ExceptionParametrosInvalidos("El modelo no puede ser null.");
+        this.modelo = modelo;
+        if (matricula == null) throw new ExceptionParametrosInvalidos("La matrícula no puede ser null.");
+        this.matricula = matricula;
+        this.precioCompra = precioCompra;
+        if (precioVenta <= precioCompra)
+            throw new ExceptionParametrosInvalidos("El precio de venta no puede ser inferior o igual al precio de compra.");
+        this.precioVenta = precioVenta;
+        if (tipo != "turismo" || tipo != "industrial" || tipo != "todoterreno")
+            throw new ExceptionParametrosInvalidos("Los tipos de vehículos permitidos son: 'turismo', 'industrial' o 'todoterreno'");
+        this.tipo = tipo;
+        if (exposicion == null) throw new ExceptionParametrosInvalidos("La exposicion no existe.");
+        this.exposicion = exposicion;
+    }
 }
--- src/com/company/Exposicion.java
+++ src/com/company/Exposicion.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class Exposicion {
+    private int numExposicion;
+    private String direccion;
+    private int telefono;
+
+    public Exposicion(int numExposicion, String direccion, int telefono) throws ExceptionParametrosInvalidos {
+        if (numExposicion == 0) throw new ExceptionParametrosInvalidos("El número de la exposición no puede ser 0.");
+        this.numExposicion = numExposicion;
+        if (direccion == null) throw new ExceptionParametrosInvalidos("La dirección no puede ser null.");
+        this.direccion = direccion;
+        if (Integer.toString(telefono).length() != 9) throw new ExceptionParametrosInvalidos("El teléfono tiene que tener 9 dígitos.");
+        this.telefono = telefono;
+    }
 }
